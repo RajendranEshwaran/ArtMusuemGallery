f1f24cf76bf6d7ee0a046453696fee36e89cdcdb
gomathi saminathan <gomathi.gms@gmail.com>
2023-09-27 16:13:40 -0400

    Initial Commit

---
+++ Shared/ArtMusuemGalleryApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArtMusuemGalleryApp.swift
+//  Shared
+//
+//  Created by Rajendran Eshwaran on 9/27/23.
+//
+
+import SwiftUI
+
+@main
+struct ArtMusuemGalleryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
